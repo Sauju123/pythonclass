d52618aa4f6e4d9b20d6d87017fe63d13bf30447
Sauju123 <basnetkedar1999@gmail.com>
2023-12-24 14:24:19 +0545

    list implementation

---
+++ pythonlist.py
@@ -0,0 +1,30 @@
+#thislist = ["apple", "banana", "cherry"]
+#print(thislist)
+
+
+#thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+#print(thislist)
+
+#LISTLENGTHS
+
+#thislist = ["apple", "banana", "cherry"]
+#print(len(thislist))
+
+
+#Vegitable = ["Cauliflower","Potato"]
+#print(Vegitable)
+
+
+
+
+carList = ["volvo","toyota",1,2]
+another_carList = ["farari","tesla"]
+print(carList.extend(another_carList))
+print(carList)
+#carList.append('taxi')
+#print(carList[0:4])
+#print(carList[-2:-1])
+#carList[1] = 'Farari'
+#print(carList)
+#print(len(carList))
+#print(help(carList))
